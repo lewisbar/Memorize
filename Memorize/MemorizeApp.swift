12a14483cc4db4f55220a4d71c0c886cc0ffc6e5
Lennart Wisbar <lewisbar.git@gmail.com>
2021-10-21 22:54:02 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Lennart Wisbar on 21.10.21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
