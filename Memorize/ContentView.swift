943fb7be1b4b9b9d301249658e7fa3c16257d453
Lennart Wisbar <lewisbar.git@gmail.com>
2021-11-04 14:31:25 +0100

    Removed game title

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -14,23 +14,20 @@ struct ContentView: View {
     @ObservedObject var viewModel: EmojiMemoryGame
     
     var body: some View {
-        VStack {
-            Text("Memorize!").font(.largeTitle).padding(.top)
-            ScrollView {
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
-                    ForEach(viewModel.cards) { card in
-                        CardView(card: card)
-                            .aspectRatio(2/3, contentMode: .fit)
-                            .onTapGesture {
-                                viewModel.choose(card)
-                            }
-                    }
+        ScrollView {
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                ForEach(viewModel.cards) { card in
+                    CardView(card: card)
+                        .aspectRatio(2/3, contentMode: .fit)
+                        .onTapGesture {
+                            viewModel.choose(card)
+                        }
                 }
-                .padding()
             }
-            .foregroundColor(.red)
-            .padding(.horizontal)
+            .padding()
         }
+        .foregroundColor(.red)
+        .padding(.horizontal)
     }
 }
 
